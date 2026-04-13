a52e9045067c18fcce9ae677627ea5b5d68a97f3
eagdev2001 <estibenacunagarita@gmail.com>
2026-04-13 12:23:00 -0600

    Se agrega el registro de usuarios, hay que pulir mas este apartado ya que no crea las tablas que deberia, pendiente el css para mejorar el apartado visual

--- Proyecto Tienda Virtual/Migrations/20260413171802_IdentityFix.Designer.cs
+++ Proyecto Tienda Virtual/Migrations/20260413171802_IdentityFix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_Tienda_Virtual.Data;
 
@@ -11,9 +12,11 @@ using Proyecto_Tienda_Virtual.Data;
 namespace Proyecto_Tienda_Virtual.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260413171802_IdentityFix")]
+    partial class IdentityFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
